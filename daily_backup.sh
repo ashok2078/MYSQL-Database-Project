3c3e6990682e2214ab2b2bf44bda9fe7e6dd3a52
ashok2078 <ashok.ak78@gmail.com>
2026-03-12 20:37:47 +0530

    Update daily_backup.sh

--- daily_backup.sh
+++ daily_backup.sh
@@ -2,7 +2,6 @@
 DEST="/root/mysql_backups"
 DATE=$(date +%F)
 
-# Ab yahan password likhne ki zaroorat nahi, script apne aap .my.cnf se utha legi
 mysqldump ecommerce_db > $DEST/ecommerce_db_$DATE.sql
 
 find $DEST -type f -mtime +7 -name "*.sql" -delete
