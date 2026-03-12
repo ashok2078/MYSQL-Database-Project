8a86b2edd302fac8fb20db8b95eca2cce934b31c
ashok2078 <ashok.ak78@gmail.com>
2026-03-12 20:46:16 +0530

    Update daily_backup.sh

--- daily_backup.sh
+++ daily_backup.sh
@@ -5,3 +5,22 @@ DATE=$(date +%F)
 mysqldump ecommerce_db > $DEST/ecommerce_db_$DATE.sql
 
 find $DEST -type f -mtime +7 -name "*.sql" -delete
+systemctl start mariadb
+systemctl list-unit-files | grep -iE "mysql|mariadb"
+yum install mariadb-server -y
+systemctl enable --now mariadb
+yum install mariadb-server -y
+systemctl start mariadb
+systemctl enable mariadb
+systemctl daemon-reload
+systemctl enable --now mysqld
+systemctl status mysqld
+mysql_secure_installation
+mysql -u root -p
+CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'MyPassword@123';
+CREATE DATABASE my_first_db;
+SELECT user, host FROM mysql.user;
+SHOW DATABASES;
+GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'myuser'@'localhost';
